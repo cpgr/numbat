77234264c998b00baa5a496c045de1df9346e3d0
Chris Green <chris.green@csiro.au>
2018-06-21 10:56:13 +1000

    Update build_and_deploy_documentation

--- docs/build_and_deploy_documentation.sh
+++ docs/build_and_deploy_documentation.sh
@@ -8,11 +8,13 @@
 
 # Build the MooseDocs site
 echo "Building site with MooseDocs"
-./moosedocs.py build
+./moosedocs.py build --clean --destination ../site
 
-# Then, use ghp-import to move the site to the gh-pages branch, and deploy it to GitHub
 # The git hash
 gvh=`git log -1 --format="%h"`
 
+# Then, use ghp-import to move the site to the gh-pages branch, and deploy it to GitHub
+# ghp-import can be installed using `pip install ghp-import`. For details, see
+# https://github.com/davisp/ghp-import
 echo "Deploying site to gh-pages"
 ghp-import -m"Deploy $gvh documentation" -p ../site
